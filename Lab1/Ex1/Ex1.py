9dd7d478352e306f8b969fa5a3221f86d660a568
ArtemEvstafev <vur1artem1cheb@gmail.com>
2021-10-07 23:53:08 +0300

    Сделал Ex2

--- Lab1/Ex1/Ex1.py
+++ Lab1/Ex1/Ex1.py
@@ -1,27 +1,33 @@
 import matplotlib.pyplot as plt
 import math
-scale_x = math.cos(math.asin(1080/1980))*15.6
-scale_y = math.sin(math.asin(1080/1980))*15.6
+
+scale_x = math.cos(math.asin(1080 / 1980)) * 15.6
+scale_y = math.sin(math.asin(1080 / 1980)) * 15.6
+
+
 def plotting(x, y):
     fig, ax = plt.subplots()
     fig.set_figheight(scale_y)
     fig.set_figwidth(scale_x)
-    if(len(x) > 100):
+    plt.axis()
+    if (len(x) > 100):
         ax.plot(x, y, 'r.', markersize=0.5)
     else:
         ax.plot(x, y, 'r.', markersize=10)
     ax.set_title("Plot № %d" % i)
     plt.savefig("Plot № %d.jpg" % i, dpi=800)
+
+
 for i in range(1, 6):
     f = open(f"dead_moroz/00{i}.dat", 'r')
     x = []
     y = []
     a = [i.rstrip() for i in f.readlines()]
-    a[int(a[0])+1::] = []
+    a[int(a[0]) + 1::] = []
     del a[0]
     for j in a:
         x.append(float(j.split()[0]))
         y.append(float(j.split()[1]))
-    a=[]
+    a = []
     plotting(x, y)
     f.close()
---
+++ Lab1/Ex2/Ex2.py
@@ -0,0 +1,41 @@
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+
+def animate(i):
+    plt.title(f'Frame {i+1}')
+    line.set_ydata([float(i) for i in a[1 + 2 * i].split()])
+    return line,
+
+
+def decorations():
+    fig.set_figheight(7)
+    fig.set_figwidth(14)
+    ax.axis([0, 17, -12, 12])
+    ax.set_xticks(range(0,17))
+    ax.set_yticks(range(-12, 12))
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    ax.grid(True)
+
+
+
+frame = 0
+f = open(f"frames.dat.txt", 'r')
+a = [i.rstrip() for i in f.readlines()]
+f.close()
+
+fig, ax = plt.subplots()
+decorations()
+x = [float(i) for i in a[0].split()]
+y = [float(i) for i in a[1].split()]
+line, = ax.plot(x, y)
+
+ani = animation.FuncAnimation(
+    fig, animate, interval=700, blit=False, frames=6)
+
+
+ani.save("plot.gif")
+
+
+plt.show()
